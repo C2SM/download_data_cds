50f1262a14a92ccaf114f26f9f21ce201815fd12
Ruth Lorenz <14321837+ruthlorenz@users.noreply.github.com>
2023-10-13 14:40:33 +0200

    only remove netcdf with all variables once all individual variables were extracted and saved.

--- era5_cds_hourly_single_levels_bymonth.py
+++ era5_cds_hourly_single_levels_bymonth.py
@@ -10,6 +10,7 @@ startyr=2022
 endyr=2022
 month_list=['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 path=f'/net/atmos/data/era5_cds/original/'
+overwrite=False
 
 # -------------------------------------------------
 # Getting libraries and utilities
@@ -69,43 +70,45 @@ for year in range(startyr, endyr+1):
     for month in month_list:
         archive=f'{grib_path}/{year}'
         if (os.access(archive, os.F_OK) == False):
-            os.makedirs(archive)   
-        c.retrieve(
-            'reanalysis-era5-single-levels',
-            {
-                'product_type': 'reanalysis',
-                'format': 'grib',
-                'variable': long_names,
-                'year': f'{year}',
-                'month': f'{month}',
-                'day': [
-                    '01', '02', '03',
-                    '04', '05', '06',
-                    '07', '08', '09',
-                    '10', '11', '12',
-                    '13', '14', '15',
-                    '16', '17', '18',
-                    '19', '20', '21',
-                    '22', '23', '24',
-                    '25', '26', '27',
-                    '28', '29', '30',
-                    '31',
-                ],
-                'time': [
-                    '00:00', '01:00', '02:00',
-                    '03:00', '04:00', '05:00',
-                    '06:00', '07:00', '08:00',
-                    '09:00', '10:00', '11:00',
-                    '12:00', '13:00', '14:00',
-                    '15:00', '16:00', '17:00',
-                    '18:00', '19:00', '20:00',
-                    '21:00', '22:00', '23:00',
-                ],
-            },
-        f'{archive}/variables_1hr_era5_{year}{month}.grib')
+            os.makedirs(archive)
+        grib_file = f'{archive}/variables_1hr_era5_{year}{month}.grib'
+        if not os.path.isfile(f'{grib_file}') or overwrite:   
+            c.retrieve(
+                'reanalysis-era5-single-levels',
+                {
+                    'product_type': 'reanalysis',
+                    'format': 'grib',
+                    'variable': long_names,
+                    'year': f'{year}',
+                    'month': f'{month}',
+                    'day': [
+                        '01', '02', '03',
+                        '04', '05', '06',
+                        '07', '08', '09',
+                        '10', '11', '12',
+                        '13', '14', '15',
+                        '16', '17', '18',
+                        '19', '20', '21',
+                        '22', '23', '24',
+                        '25', '26', '27',
+                        '28', '29', '30',
+                        '31',
+                    ],
+                    'time': [
+                        '00:00', '01:00', '02:00',
+                        '03:00', '04:00', '05:00',
+                        '06:00', '07:00', '08:00',
+                        '09:00', '10:00', '11:00',
+                        '12:00', '13:00', '14:00',
+                        '15:00', '16:00', '17:00',
+                        '18:00', '19:00', '20:00',
+                        '21:00', '22:00', '23:00',
+                    ],
+                },
+            f'{grib_file}')
 
         # convert grib file to netcdf
-        os.system(f'cdo -f nc copy {archive}/variables_1hr_era5_{year}{month}.grib {workdir}/variables_1hr_era5_{year}{month}.nc')
+        os.system(f'cdo -f nc copy {grib_file} {workdir}/variables_1hr_era5_{year}{month}.nc')
 
         # extract individual variables and change metadata
         for v, var in enumerate(variables):
@@ -116,4 +119,4 @@ for year in range(startyr, endyr+1):
             os.system(f'ncatted -a standard_name,{old_names[v]},c,c,{long_names[v]} {workdir}/{old_names[v]}_1hr_era5_{year}{month}.nc {workdir}/{old_names[v]}_1hr_era5_{year}{month}_ncatted.nc')
             os.system(f'ncatted -a units,{old_names[v]},c,c,{units} {workdir}/{old_names[v]}_1hr_era5_{year}{month}_ncatted.nc {workdir}/{old_names[v]}_1hr_era5_{year}{month}_ncatted2.nc')
             os.system(f'cdo setname,{var} {workdir}/{old_names[v]}_1hr_era5_{year}{month}.nc {path_out}/{var}_1hr_era5_{year}{month}.nc')
-            os.system(f'rm {workdir}/*')
+        os.system(f'rm {workdir}/*')
