f5a319d3acda6c6a56fda06dcd5724687c1467d6
Ruth Lorenz <ruth.lorenz@c2sm.ethz.ch>
2023-12-22 10:25:36 +0100

    fix folder where data is saved on atmos (correct variable names in path), month is string and need to be int for calendar

--- era5_cds_hourly_pressure_levels_byday_convert_day_cmip.py
+++ era5_cds_hourly_pressure_levels_byday_convert_day_cmip.py
@@ -4,7 +4,7 @@
 #                         U S E R  *  O P T I O N S
 # *******************************************************************************
 
-variables = ['t']
+variables = ['t', 'u', 'v', 'r', 'cc', 'z']
 
 startyr=1980
 endyr=1980
@@ -25,7 +25,7 @@ import json
 import cdsapi
 import logging
 import calendar
-import xarray
+import xarray as xr
 import glob
 import socket
 
@@ -170,8 +170,8 @@ def main():
                     print(f'{workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}.nc')
                     os.system(f'ncks -O -v {old_names[v]} {workdir}/Z_1hr_era5_{year}{month}{day_str}.nc {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}.nc')
 
-                    os.system(f'ncatted -a long_name,{old_names[v]},c,c,"{long_names[v]}" {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}.nc {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}_ncatted.nc')
-                    os.system(f'ncatted -a units,{old_names[v]},c,c,"{units[v]}" {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}_ncatted.nc {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}_ncatted2.nc')
+                    os.system(f'ncatted -O -a long_name,{old_names[v]},c,c,"{long_names[v]}" {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}.nc {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}_ncatted.nc')
+                    os.system(f'ncatted -O -a units,{old_names[v]},c,c,"{units[v]}" {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}_ncatted.nc {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}_ncatted2.nc')
                     os.system(f'cdo setname,{varname} {workdir}/{old_names[v]}_1hr_era5_{year}{month}{day_str}.nc {workdir}/{varname}_1hr_era5_{year}{month}{day_str}.nc')
 
                     # check if unit needs to be changed from era5 variable to cmip variable
@@ -209,28 +209,29 @@ def main():
 
 
         # concatenate daily files for each month and convert to cmip standards
-        proc_archive=f'{path_proc}/{varname}/day/native/{year}'
-        os.makedirs(proc_archive, exist_ok=True)
 
         for v, varname in enumerate(variables):
+            proc_archive=f'{path_proc}/{cmip_names[v]}/day/native/{year}'
+            os.makedirs(proc_archive, exist_ok=True)
+
             outfile=f'{proc_archive}/{cmip_names[v]}_day_era5_{year}{month}.nc'
             name_day_work=f'{workdir}/{varname}_daymean_era5_{year}{month}'
 
             # check if all days for this month are available
             daily_files = glob.glob(f'{name_day_work}??.nc')
-            num_days_month = calendar.monthrange(year, month)[1]
+            num_days_month = calendar.monthrange(year, int(month))[1]
             if (len(daily_files) != num_days_month):
                 logger.error(f'Not all files for year {year} and month {month} are there!')
                 sys.exit(1)
             else:
-                os.system(f'cdo timemerge {name_day_work}??.nc {name_day_work}.nc')
+                os.system(f'cdo mergetime {name_day_work}??.nc {name_day_work}.nc')
 
             # extract number of p-levels for chunking
             with xr.open_dataset(f'{name_day_work}.nc') as ds:
                 plev = ds.sizes['plev']
 
-            os.system(f'ncatted -O -h -a comment,global,m,c,"Daily data aggregated as mean over calendar day 00:00:00 to 23:00:00." {name_day_work}.nc {name_day_work}_ncatted.nc')
-            os.system(f'ncks -O -4 -D 4 --cnk_plc=g3d --cnk_dmn=time,1 --cnk_dmn=plev,{plev} --cnk_dmn=lat,{lat_ck} --cnk_dmn=lon,{lon_ck} {name_day_work}_ncatted.nc {name_day_work}_chunked.nc')
+            os.system(f'ncatted -O -h -a comment,global,c,c,"Daily data aggregated as mean over calendar day 00:00:00 to 23:00:00." {name_day_work}.nc {name_day_work}_ncatted.nc')
+            os.system(f'ncks -O -4 -D 4 --cnk_plc=g3d --cnk_dmn=time,1 --cnk_dmn=plev,{plev} --cnk_dmn=lat,{lat_chk} --cnk_dmn=lon,{lon_chk} {name_day_work}_ncatted.nc {name_day_work}_chunked.nc')
             os.system(f'ncrename -v {varname},{cmip_names[v]} {name_day_work}_chunked.nc {outfile}')
 
         #os.system(f'rm {workdir}/*')
